c02b588478a5c2c287299ee1e543bcb80959f8ed
Matthew LaCorte <mretrocal@gmail.com>
2025-08-04 02:03:06 -0700

    fix: update config file path in benfords_law script and add visualization

--- scripts/fractals/benfords_law/benfords_law.py
+++ scripts/fractals/benfords_law/benfords_law.py
@@ -115,5 +115,5 @@ def run_benford_analysis(config_path):
 
 if __name__ == '__main__':
     # This points to the config file defining the experiment
-    config_file = 'experiments/scripts/prime_isomorphism/benfords_law/config/benford_analysis_config.yml'
+    config_file = 'scripts/fractals/benfords_law/config/benford_analysis_config.yml'
     run_benford_analysis(config_file)
